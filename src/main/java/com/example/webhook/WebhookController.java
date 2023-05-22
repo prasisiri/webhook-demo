16c437a02b79c8a4d05f52c4f70bf9d91ff38770
prasisiri <prasanna.iitr@gmail.com>
2023-05-22 08:21:54 -0500

    added swagger

--- src/main/java/com/example/webhook/WebhookController.java
+++ src/main/java/com/example/webhook/WebhookController.java
@@ -1,21 +1,27 @@
 package com.example.webhook;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/webhook")
+@Api(value = "Webhook Controller", description = "Operations with Webhooks")
 public class WebhookController {
 
-    @PostMapping
-    public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
-        // do something with the payload
-        System.out.println(payload);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebhookController.class);
 
+    @PostMapping
+    @ApiOperation(value = "Handle incoming webhook", response = String.class)
+    public ResponseEntity<String> handleWebhook(@ApiParam(value = "Webhook payload", required = true) @RequestBody String payload) {
+        LOGGER.info(payload);
         return ResponseEntity.ok().body("Webhook received successfully");
     }
 }
-
